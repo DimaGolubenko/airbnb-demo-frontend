d530902a0b1ef57c9f059aebd2b953915aad4ed3
DimaGolubenko <dimagolubenko@gmail.com>
2018-01-05 05:22:03 +0200

    Fix Explore component. Remove borders under images.

--- src/Main/Explore/index.js
+++ src/Main/Explore/index.js
@@ -8,22 +8,31 @@ import img3 from "./Explore-3.png";
 const Explore = styled.section``;
 
 const Card = styled.a`
+  position: relative;
   display: flex;
   align-items: center;
+  box-sizing: border-box;
+  height: 4.5rem;
   text-decoration: none;
   border: 1px solid rgba(72, 72, 72, 0.2);
+  border-radius: 4px;
 `;
 
 const Image = styled.img`
+  position: absolute;
+  top: -1px;
+  left: -1px;
   width: 6rem;
   height: 4.5rem;
   margin-right: 1.5rem;
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
 `;
 
 const CardTitle = styled.h2`
+  padding-left: 7.5rem;
   font-family: "CircularAirBold", "Helvetica Neue", Helvetica, Arial, sans-serif;
   font-size: 1rem;
-  line-height: 4.5rem;
   color: #383838;
 `;
 
