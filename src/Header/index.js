9483b8749c82a66ad780cd3bdfa6f1381b26d30e
DimaGolubenko <dimagolubenko@gmail.com>
2018-01-06 21:45:47 +0200

    Fix naming of components for Header

--- src/Header/index.js
+++ src/Header/index.js
@@ -4,9 +4,7 @@ import Logo from "./Logo";
 import SearchForm from "./SearchForm";
 import Nav from "./Nav";
 
-const Wrapper = styled.div``;
-
-const Header = styled.header`
+const Wrapper = styled.div`
   padding: 1rem 0;
   display: flex;
   width: 100%;
@@ -15,16 +13,18 @@ const Header = styled.header`
   box-shadow: 0px 0.5px 0px rgba(72, 72, 72, 0.3);
 `;
 
+const Header = styled.header``;
+
 export default () => {
   return (
-    <Wrapper>
+    <Header>
       <div className="container">
-        <Header>
+        <Wrapper>
           <Logo />
           <SearchForm />
           <Nav />
-        </Header>
+        </Wrapper>
       </div>
-    </Wrapper>
+    </Header>
   );
 };
