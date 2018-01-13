156f6d62ac97310074c68ca4fb2e67e22e8ced14
Dima Golubenko <dimagolubenko@gmail.com>
2018-01-13 15:20:20 +0200

    Fix naming of attribute in Pagination component

--- src/Homes/Pagination.js
+++ src/Homes/Pagination.js
@@ -25,8 +25,8 @@ const PageNumber = styled.span`
   font-size: 1rem;
   line-height: 2rem;
   text-align: center;
-  color: ${props => (props.isCurrent ? "#fff" : "#0f7276")};
-  background-color: ${props => (props.isCurrent ? "#0f7276" : "transparent")};
+  color: ${props => (props.active ? "#fff" : "#0f7276")};
+  background-color: ${props => (props.active ? "#0f7276" : "transparent")};
   border-radius: 1rem;
   cursor: pointer;
 
@@ -61,7 +61,7 @@ const Count = styled.p`
 export default () => (
   <Pagination>
     <Pages>
-      <PageNumber isCurrent="true">1</PageNumber>
+      <PageNumber active={true}>1</PageNumber>
       <PageNumber>2</PageNumber>
       <PageNumber>3</PageNumber>
       <PageNumber>...</PageNumber>
